e59aa460b21be2a0be59577bcdae470a15371ed7
DamunGames <96812158+DamunGames@users.noreply.github.com>
2024-09-30 20:05:36 +0900

    アイコン用キャプチャー撮影用シーン追加 スクリーンショット撮影機能追加

---
+++ Assets/DualFPSViewer/Scripts/Editor/ScreenShot.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class ScreenShot
+{
+	const string OutputDirectory = "Screenshots";
+	const string FileName = "Screenshot";
+
+	[MenuItem("Tools/Screenshot")]
+	public static void CaptureScreenshot()
+	{
+		if (!Directory.Exists(OutputDirectory)) {
+			Directory.CreateDirectory(OutputDirectory);
+		}
+
+		DateTime dateTime = DateTime.Now;
+		string fileName = $"{FileName}_{dateTime.Year}_{dateTime.Month}_{dateTime.Day}_{dateTime.Hour}_{dateTime.Minute}_{dateTime.Second}.png";
+		string outputPath = Path.Combine(OutputDirectory, fileName);
+
+		ScreenCapture.CaptureScreenshot(outputPath);
+		Debug.Log($"Captured Screenshot path: {outputPath}");
+	}
+}
+#endif
